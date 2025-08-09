9787a50593d4eb1a9a15554c3d4b5fc885e67c19
Deepika <deepikavichare2109@gmail.com>
2025-08-09 15:37:07 +0530

    27391

--- home/views.py
+++ home/views.py
@@ -5,8 +5,16 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def menu_items(request):
     items = [
-        {"id":1, name:"Cheese Corn Pizza", price:"250"},
-        {"id":2, name:"Cheese Dosa", price:"180"},
-        {"id":3, name:"Sizzling Brownie", price:"220"}
+        {"id":1, "name":"Cheese Corn Pizza", "price":"250"},
+        {"id":2, "name":"Cheese Dosa", "price":"180"},
+        {"id":3, "name":"Sizzling Brownie", "price":"220"}
     ]
     return Response(items)
+
+def contact_us(request):
+    contact_info:{
+        "phone":"+91 1023456789",
+        "email":"resto@gmail.com",
+        "address":"151, Rose Building, Lili Road"
+    }
+    return render(request,"contact,html",{"contact_info": contact_info})
