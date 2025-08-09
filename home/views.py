6d8e3cc0b0112ac680244a240b2eed78e330bb4a
Deepika <deepikavichare2109@gmail.com>
2025-08-09 09:01:30 +0530

    27389

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+@api_view(['GET'])
+def menu_items(request):
+    items = [
+        {"id":1, name:"Cheese Corn Pizza", price:"250"},
+        {"id":2, name:"Cheese Dosa", price:"180"},
+        {"id":3, name:"Sizzling Brownie", price:"220"}
+    ]
+    return Response(items)
